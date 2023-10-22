69474f92d1bae120c9e0bc510eb495d395115443
Rocio <rociocor77@gmail.com>
2023-10-22 09:02:24 +0200

    EJ01

---
+++ EJ01.java
@@ -0,0 +1,12 @@
+package Capitulo5;
+/*Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.*/
+public class EJ01 {
+    public static void main(String[] args) {
+        for (int i = 5; i <=100; i+=5) {
+            /*if(i==6)
+                break;//El break rompe, entonces termina en 6
+             */
+            System.out.println(i);
+        }
+    }
+}
