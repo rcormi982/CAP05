3d0625b9846a5fd4a13b0a9b57919ff17bccb84b
Rocio <rociocor77@gmail.com>
2023-10-30 09:44:37 +0100

    EJ24

---
+++ EJ24.java
@@ -0,0 +1,30 @@
+package Capitulo5;
+
+public class EJ24 {
+    public static void main(String[] args) {
+        System.out.println("Este programa pinta una pirámide hecha a base de números.");
+        System.out.print("Por favor, introduzca la altura de la pirámide:");
+        int alturaIntroducida = Integer.parseInt(System.console().readLine());
+        int altura = 1;
+        int i = 0;
+        int espacios = alturaIntroducida - 1;
+        while (altura <= alturaIntroducida) {
+        // inserta espacios
+            for (i = 1; i <= espacios; i++) {
+                System.out.print(" ");
+        }
+        // pinta la línea de números
+            for (i = 1; i < altura; i++) {
+                System.out.print(i);
+            }
+            for (i = altura; i > 0; i--) {
+                System.out.print(i);
+            }
+            System.out.println();
+            altura++;
+            espacios--;
+        }
+
+
+    }
+}
